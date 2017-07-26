83047f0ab9d242eeda40c6a5314ff24d88530aff
Abdurakhmanov Artur <abdurahmanovart1994@gmail.com>
2017-07-26 23:42:05 +0300

    added menu, adapter and basic fragment files

--- app/src/main/java/com/github/abdurahmanovart/notesrealm/MainActivity.java
+++ app/src/main/java/com/github/abdurahmanovart/notesrealm/MainActivity.java
@@ -1,13 +1,25 @@
 package com.github.abdurahmanovart.notesrealm;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import com.github.abdurahmanovart.notesrealm.adapter.CustomPagerAdapter;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+    private TabLayout mTabLayout;
+    private ViewPager mViewPager;
+    private CustomPagerAdapter mAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
---
+++ app/src/main/java/com/github/abdurahmanovart/notesrealm/adapter/CustomPagerAdapter.java
@@ -0,0 +1,43 @@
+package com.github.abdurahmanovart.notesrealm.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import com.github.abdurahmanovart.notesrealm.BasicFragment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Abdurakhmanov on 26.07.17
+ */
+
+public class CustomPagerAdapter extends FragmentStatePagerAdapter{
+
+    List<BasicFragment> mFragmentList;
+
+    public CustomPagerAdapter(FragmentManager fm) {
+        super(fm);
+        mFragmentList = new ArrayList<>();
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return mFragmentList.isEmpty() ? null : mFragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragmentList.size();
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mFragmentList.get(position).getTitle();
+    }
+
+    public void addFragment(BasicFragment fragment){
+        mFragmentList.add(fragment);
+    }
+}
